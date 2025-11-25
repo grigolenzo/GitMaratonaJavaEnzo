82f8c7f58c4eec8ce98a6bf21e082f098a04462c
grigolenzo <enzogmartins@gmail.com>
2025-11-25 19:37:14 -0300

    operadores ternários

--- src/OperadoresTernario.java
+++ src/OperadoresTernario.java
@@ -1,7 +1,9 @@
 public class OperadoresTernario {
     public static void main(String[] args) {
         double salario = 6000;
-        String resultado = salario > 5000 ? "Eu vou doar 500 para o dev dojo!" : "Não consigo doar ainda, mas vou poder!";
+        String resultado = salario > 5000 ? "Eu vou guardar 500 reais" : "Eu vou aumentar aumentar minha renda para investir mais!";
+
+
 
         System.out.println(resultado);
 
