349fb197a3a0d17876f861920e37d243299c0d49
grigolenzo <enzogmartins@gmail.com>
2025-11-25 18:11:22 -0300

    primeiro commit

---
+++ src/OperadoresTernario.java
@@ -0,0 +1,10 @@
+public class OperadoresTernario {
+    public static void main(String[] args) {
+        double salario = 6000;
+        String resultado = salario > 5000 ? "Eu vou doar 500 para o dev dojo!" : "Não consigo doar ainda, mas vou poder!";
+
+        System.out.println(resultado);
+
+
+    }
+}
