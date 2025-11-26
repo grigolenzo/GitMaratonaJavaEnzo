d53a2323b4d62d14f9af8cf199510cfe4adc2649
grigolenzo <enzogmartins@gmail.com>
2025-11-26 00:24:12 -0300

    tabela verdade e exercicio para fazer

---
+++ src/TabelaVerdade.java
@@ -0,0 +1,31 @@
+public class TabelaVerdade {
+    /*
+    * Tabela Verdade
+    *
+    * tabela &&
+    *
+    * Todas as instruções no if precisam ser verdadeira para que o resultado final seja verdadeiro
+    *
+    * V && V = V
+    * V && F = F
+    * F && V = F
+    * F && F = F
+    *
+    * PODE TER INUMERAS CONDIÇÕES A UNICA QUE SERÁ VERDADEIRA NA TABELA && É QUANDO AMBAS CONDIÇOES SÃO VERDADEIRAS
+    *
+    * V && V && V && V && F = F
+    * V && V && V && V && V = V
+    *
+    * tabela ||
+    * O CONTRÁRIO É APLICADO PARA O ||
+    *
+    * V || V = V
+    * V || F = V
+    * F || V = V
+    * F || F = F
+    *
+    * USANDO O || SÓ SERÁ RESULTADO EM FALSO QUNADO TODAS AS CONDIÇÕES FOREM FALSAS.
+    *
+    *
+    * */
+}
